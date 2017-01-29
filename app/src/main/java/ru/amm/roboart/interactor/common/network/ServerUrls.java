1052afc30fe51478785d3e823c35f5888b584543
saturov <saturov@surfstudio.ru>
2017-01-29 17:00:18 +0300

    Модуль ретрофита

---
+++ app/src/main/java/ru/amm/roboart/interactor/common/network/ServerUrls.java
@@ -0,0 +1,8 @@
+package ru.amm.roboart.interactor.common.network;
+
+/**
+ * url запросов сервера
+ */
+public class ServerUrls {
+    public static final String BASE_URL = "http://private-f2447e-roboart.apiary-mock.com";
+}
